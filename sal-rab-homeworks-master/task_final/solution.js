039a7665497ee12e9f9fbeb6761cb0f514d9292a
Nansy0209 <108424095+Nansy0209@users.noreply.github.com>
2022-09-13 13:18:57 +0500

    Add files via upload
    Task final commit

--- sal-rab-homeworks-master/task_final/solution.js
+++ sal-rab-homeworks-master/task_final/solution.js
@@ -34,20 +34,28 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {client, order: {}, goods: []};
+    
+    let arrayGoods = [];
 
-    let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < goods.length; i += 1) {
+        arrayGoods.push({
+            title: goods[i]["title"],
+            count: goods[i]["count"]
+        });
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
+    let data = {
+        client: `${name} ${phone}`,
+        order: {
+            address: `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`,
+            sum: sum
+        },
+        goods: arrayGoods
+    };
+    let objectData = {
+        data: data
+    }
+    let jsonData = JSON.stringify(objectData);
+    console.log(jsonData);
     return jsonData;
 }
